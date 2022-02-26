859f5997dbb134c66f71340f18d81b29ccf47963
Vahe1232 <vahe.abov@gmail.com>
2022-02-26 12:25:06 +0400

    final

--- src/components/QuoteContainer.vue
+++ src/components/QuoteContainer.vue
@@ -1,10 +1,115 @@
 <template>
   <div class="quote">
-    <span class="quote__number">Advice # {{ number }}</span>
-    <div class="quote__text">Txt {{ Text }}</div>
+    <transition name="fade">
+      <div class="quote__content" v-if="quote">
+        <span class="quote__number">Advice # {{ quote.id }}</span>
+        <blockquote class="quote__text">"{{ quote.advice }}"</blockquote>
+      </div>
+    </transition>
+
+    <div class="quote__elements">
+      <img
+        class="quote-devider desktop"
+        src="../assets/images/pattern-divider-desktop.svg"
+        alt=""
+      />
+      <img
+        class="quote-devider mobile"
+        src="../assets/images/pattern-divider-mobile.svg"
+        alt=""
+      />
+    </div>
+    <div class="quote__button">
+      <button class="btn" @click="getRandomAdvice">
+        <img src="../assets/images/icon-dice.svg" alt="" />
+      </button>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue';
 
-<style langs="scss"></style>
+const quote = ref(null);
+const isLoading = ref(false);
+
+const getRandomAdvice = async () => {
+  isLoading.value = true;
+  const response = await fetch('https://api.adviceslip.com/advice');
+  const data = await response.json();
+  quote.value = data.slip;
+  isLoading.value = false;
+};
+
+onMounted(async () => {
+  await getRandomAdvice();
+});
+</script>
+
+<style lang="scss">
+.quote {
+  background-color: rgba($color: $grayishBlue, $alpha: 0.5);
+  padding: 50px;
+  border-radius: 10px;
+  text-align: center;
+  position: relative;
+
+  &__number {
+    color: $neonGreen;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-size: 14px;
+    display: inline-block;
+    padding-bottom: 40px;
+
+    @include breakpoint(small) {
+      font-size: 13px;
+    }
+  }
+  &__text {
+    color: $lightCyan;
+    font-size: 28px;
+    padding-bottom: 40px;
+
+    @include breakpoint(small) {
+      font-size: 24pxpx;
+    }
+  }
+  &__elements {
+    width: 100%;
+    padding-bottom: 20px;
+
+    .mobile {
+      display: none;
+    }
+
+    @include breakpoint(small) {
+      .desktop {
+        display: none;
+      }
+      .mobile {
+        display: block;
+      }
+    }
+  }
+  &__button {
+    position: absolute;
+    left: 50%;
+    bottom: -48px;
+    transform: translate(-50%, -50%);
+
+    .btn {
+      border: none;
+      background: $neonGreen;
+      padding: 15px;
+      border-radius: 50%;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        box-shadow: 0 0 30px 15px $neonGreen;
+      }
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   css: {
     loaderOptions: {
       sass: {
-        additionalData: `@import "@/assets/scss/style.scss"; @import "@/assets/scss/colors.scss"; @import "@/assets/scss/reset.scss"; @import "@/assets/scss/breakpoints.scss";`
+        additionalData: `@import "@/assets/scss/style.scss"; @import "@/assets/scss/colors.scss"; @import "@/assets/scss/reset.scss"; @import "@/assets/scss/breakpoints.scss";  @import "@/assets/scss/transitions.scss";`
       }
     }
   }
